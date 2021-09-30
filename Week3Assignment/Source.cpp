a72ea4a503f1a60c5d76e3699ea366143eb9f9da
peterrw9000 <peterrw9001@yahoo.com>
2021-09-29 21:11:27 -0500

    broken code
    I have no idea what to fix

--- Week3Assignment/Source.cpp
+++ Week3Assignment/Source.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 using namespace std;
@@ -50,24 +51,68 @@ public:
 
 int main()
 {
-	ifstream myFile("test.txt");
+	ifstream myFile("example.txt");
 	vector<Student> fooStudents;
-	vector<string> studentParts;
 	string student;
 	if (myFile.is_open())
 	{
+		int locOfSpace;
 		while (getline(myFile, student))
 		{
-			//studentParts.push_back(student);
-			cout << student << endl;
+			int count = 0;
+			string part2;
+			string part3;
+			string first;
+			string last;
+			string gr;
+			int grade;
+			locOfSpace = student.find(' ');
+			first = student.substr(0, locOfSpace);
+			part2 = student.substr(locOfSpace, student.length());
+			locOfSpace = part2.find(' ');
+			last = part2.substr(0, locOfSpace);
+			part3 = part2.substr(locOfSpace, student.length());
+			locOfSpace = part3.find(' ');
+			gr = part3.substr(0, locOfSpace);
+			try
+			{
+				grade = std::stoi(gr);
+			}
+			catch (exception e)
+			{
+				cout << "convertion errors";
+			}
+			fooStudents[count].setFirstName(first);
+			fooStudents[count].setLastName(last);
+			fooStudents[count].setGrade(grade);
+			count++;
 		}
 	}
 	else
 	{
 		cout << "unable to open";
 	}
-	/*for (int i = 0; i < studentParts.size(); i++)
+	/*for (int o = 0; o < fooStudents.size(); o++)
 	{
-		cout << studentParts[i] << endl;
+		cout << fooStudents[o].getFirstName() << " " << fooStudents[o].getLastName() << " " << fooStudents[o].getGrade() << endl;
 	}*/
+	int total = 0;
+	int sized = fooStudents.size();
+	for (int i = 0; i < sized - 1; i++)
+	{
+		for (int s = 0; s < sized - i - 1; s++)
+		{
+			if (fooStudents[s].getGrade() < fooStudents[s + 1].getGrade())
+			{
+				swap(fooStudents[s], fooStudents[s + 1]);
+			}
+		}
+	}
+	//cout << endl;
+	for (int o = 0; o < fooStudents.size(); o++)
+	{
+		cout << fooStudents[o].getFirstName() << " " << fooStudents[o].getLastName() << " "  << fooStudents[o].getGrade() << endl;
+		total = total + fooStudents[o].getGrade();
+	}
+	cout << endl << total/fooStudents.size() << endl;
 }
